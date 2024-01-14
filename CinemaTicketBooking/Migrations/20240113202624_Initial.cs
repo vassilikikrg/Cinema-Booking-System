60731d9d3cbf20fa5075bffa6fd63e06c00f7a0e
Vassiliki Karagianniki <78561945+vassilikikrg@users.noreply.github.com>
2024-01-14 16:46:11 +0200

    Initial migration and creation of the database. Fix in connection string

---
+++ CinemaTicketBooking/Migrations/20240113202624_Initial.cs
@@ -0,0 +1,247 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CinemaTicketBooking.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "cinemas",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    name = table.Column<string>(type: "varchar(255)", unicode: false, maxLength: 255, nullable: false),
+                    seats = table.Column<int>(type: "int", nullable: false),
+                    _3d = table.Column<string>(name: "3d", type: "varchar(255)", unicode: false, maxLength: 255, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_cinema_1", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "users",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    username = table.Column<string>(type: "varchar(255)", unicode: false, maxLength: 255, nullable: false),
+                    email = table.Column<string>(type: "varchar(255)", unicode: false, maxLength: 255, nullable: false),
+                    password = table.Column<string>(type: "varchar(255)", unicode: false, maxLength: 255, nullable: false),
+                    create_time = table.Column<DateTime>(type: "datetime", nullable: false),
+                    salt = table.Column<string>(type: "varchar(255)", unicode: false, maxLength: 255, nullable: false),
+                    role = table.Column<string>(type: "varchar(255)", unicode: false, maxLength: 255, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_user_1", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "admins",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    name = table.Column<string>(type: "varchar(255)", unicode: false, maxLength: 255, nullable: false),
+                    user_id = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_admin_1", x => x.id);
+                    table.ForeignKey(
+                        name: "admins_fk0",
+                        column: x => x.user_id,
+                        principalTable: "users",
+                        principalColumn: "id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "content_admin",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    name = table.Column<string>(type: "varchar(255)", unicode: false, maxLength: 255, nullable: false),
+                    user_id = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_contentAdmin_1", x => x.id);
+                    table.ForeignKey(
+                        name: "content_admin_fk0",
+                        column: x => x.user_id,
+                        principalTable: "users",
+                        principalColumn: "id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "customers",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    name = table.Column<string>(type: "varchar(255)", unicode: false, maxLength: 255, nullable: false),
+                    user_id = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_customer_1", x => x.id);
+                    table.ForeignKey(
+                        name: "customers_fk0",
+                        column: x => x.user_id,
+                        principalTable: "users",
+                        principalColumn: "id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "movies",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    name = table.Column<string>(type: "varchar(255)", unicode: false, maxLength: 255, nullable: false),
+                    content = table.Column<string>(type: "varchar(255)", unicode: false, maxLength: 255, nullable: false),
+                    length = table.Column<int>(type: "int", nullable: false),
+                    summary = table.Column<string>(type: "varchar(255)", unicode: false, maxLength: 255, nullable: false),
+                    director = table.Column<string>(type: "varchar(255)", unicode: false, maxLength: 255, nullable: false),
+                    content_admin_id = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_movie_1", x => x.id);
+                    table.ForeignKey(
+                        name: "movies_fk0",
+                        column: x => x.content_admin_id,
+                        principalTable: "content_admin",
+                        principalColumn: "id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "screenings",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    movie_id = table.Column<int>(type: "int", nullable: false),
+                    cinema_id = table.Column<int>(type: "int", nullable: false),
+                    day = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    start_time = table.Column<TimeSpan>(type: "time", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_screening_1", x => x.id);
+                    table.ForeignKey(
+                        name: "screenings_fk0",
+                        column: x => x.movie_id,
+                        principalTable: "movies",
+                        principalColumn: "id");
+                    table.ForeignKey(
+                        name: "screenings_fk1",
+                        column: x => x.cinema_id,
+                        principalTable: "cinemas",
+                        principalColumn: "id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "reservations",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    screening_id = table.Column<int>(type: "int", nullable: false),
+                    customer_id = table.Column<int>(type: "int", nullable: false),
+                    number_of_seats = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_reservation_1", x => x.id);
+                    table.ForeignKey(
+                        name: "reservations_fk0",
+                        column: x => x.screening_id,
+                        principalTable: "screenings",
+                        principalColumn: "id");
+                    table.ForeignKey(
+                        name: "reservations_fk1",
+                        column: x => x.customer_id,
+                        principalTable: "customers",
+                        principalColumn: "id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_admins_user_id",
+                table: "admins",
+                column: "user_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_content_admin_user_id",
+                table: "content_admin",
+                column: "user_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_customers_user_id",
+                table: "customers",
+                column: "user_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_movies_content_admin_id",
+                table: "movies",
+                column: "content_admin_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_reservations_customer_id",
+                table: "reservations",
+                column: "customer_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_reservations_screening_id",
+                table: "reservations",
+                column: "screening_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_screenings_cinema_id",
+                table: "screenings",
+                column: "cinema_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_screenings_movie_id",
+                table: "screenings",
+                column: "movie_id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "admins");
+
+            migrationBuilder.DropTable(
+                name: "reservations");
+
+            migrationBuilder.DropTable(
+                name: "screenings");
+
+            migrationBuilder.DropTable(
+                name: "customers");
+
+            migrationBuilder.DropTable(
+                name: "movies");
+
+            migrationBuilder.DropTable(
+                name: "cinemas");
+
+            migrationBuilder.DropTable(
+                name: "content_admin");
+
+            migrationBuilder.DropTable(
+                name: "users");
+        }
+    }
+}
