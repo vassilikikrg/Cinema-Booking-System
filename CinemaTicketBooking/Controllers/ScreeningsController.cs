88c41ac2a1adbd664b17e3dfa98bad606dcf4b40
Vassiliki Karagianniki <78561945+vassilikikrg@users.noreply.github.com>
2024-01-16 01:07:40 +0200

    Check if screening date is in future (valid) and if it already exists one in the same hall and for the same movie in a specific time

--- CinemaTicketBooking/Controllers/ScreeningsController.cs
+++ CinemaTicketBooking/Controllers/ScreeningsController.cs
@@ -61,18 +61,32 @@ namespace CinemaTicketBooking.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create([Bind("Id,MovieId,CinemaId,StartDateAndTime")] Screening screening)
         {
-            foreach (var modelStateEntry in ModelState.Values)
-            {
-                foreach (var error in modelStateEntry.Errors)
-                {
-                    Console.WriteLine(error.ErrorMessage);
-                }
-            }
+            bool error = false;
             if (ModelState.IsValid)
             {
-                _context.Screenings.Add(screening);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
+                if (!ScreeningDateTimeIsValid(screening.StartDateAndTime))
+                {
+                    ModelState.AddModelError(nameof(Screening.StartDateAndTime), "Select a future date and starting time");
+                    error = true;
+                }
+                var movie = _context.Movies.FirstOrDefault(m => m.Id == screening.MovieId);
+                if (screeningsOverlapping(screening.MovieId, screening.CinemaId, screening.StartDateAndTime, movie.Length))
+                {
+                    ModelState.AddModelError(nameof(Screening.StartDateAndTime), "There is already a screening for this movie in the specified cinema hall starting or in progress at the time and day specified");
+                    error = true;
+                }
+                if (error)
+                {
+                    ViewData["CinemaId"] = new SelectList(_context.Cinemas, "Id", "Name", screening.CinemaId);
+                    ViewData["MovieId"] = new SelectList(_context.Movies, "Id", "Name", screening.MovieId);
+                    return View(screening);
+                }
+                else
+                {
+                    _context.Screenings.Add(screening);
+                    await _context.SaveChangesAsync();
+                    return RedirectToAction(nameof(Index));
+                }
             }
             ViewData["CinemaId"] = new SelectList(_context.Cinemas, "Id", "Name", screening.CinemaId);
             ViewData["MovieId"] = new SelectList(_context.Movies, "Id", "Name", screening.MovieId);
@@ -98,8 +112,6 @@ namespace CinemaTicketBooking.Controllers
         }
 
         // POST: Screenings/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Edit(int id, [Bind("Id,MovieId,CinemaId,StartDateAndTime")] Screening screening)
@@ -113,8 +125,31 @@ namespace CinemaTicketBooking.Controllers
             {
                 try
                 {
-                    _context.Screenings.Update(screening);
-                    await _context.SaveChangesAsync();
+                    bool error = false;
+                    if (!ScreeningDateTimeIsValid(screening.StartDateAndTime))
+                    {
+                        ModelState.AddModelError(nameof(Screening.StartDateAndTime), "Select a future date and starting time");
+                        error = true;
+                    }
+                    var movie = _context.Movies.FirstOrDefault(m => m.Id == screening.MovieId);
+                    if (screeningsOverlapping(screening.MovieId, screening.CinemaId, screening.StartDateAndTime, movie.Length, screening.Id))
+                    {
+                        ModelState.AddModelError(nameof(Screening.StartDateAndTime), "There is already a screening for this movie in the specified cinema hall starting or in progress at the time and day specified");
+                        error = true;
+                    }
+
+                    if (error)
+                    {
+                        ViewData["CinemaId"] = new SelectList(_context.Cinemas, "Id", "Name", screening.CinemaId);
+                        ViewData["MovieId"] = new SelectList(_context.Movies, "Id", "Name", screening.MovieId);
+                        return View(screening);
+                    }
+                    else
+                    {
+                        _context.Screenings.Update(screening);
+                        await _context.SaveChangesAsync();
+                    }
+
                 }
                 catch (DbUpdateConcurrencyException)
                 {
@@ -172,6 +207,25 @@ namespace CinemaTicketBooking.Controllers
         private bool ScreeningExists(int id)
         {
             return _context.Screenings.Any(e => e.Id == id);
+        }
+
+        public bool ScreeningDateTimeIsValid(DateTime date)
+        {
+            return !(date < DateTime.Now);
+        }
+        public bool screeningsOverlapping(int movieId, int cinemaId, DateTime screeningDate,int movieLength)
+        {   //check if there is another screening scheduled for the same time period
+            DateTime date1=screeningDate.AddMinutes(-movieLength);
+            DateTime date2=screeningDate.AddMinutes(movieLength);
+            return _context.Screenings
+                .Any(e => e.MovieId == movieId && e.CinemaId==cinemaId && e.StartDateAndTime>date1 && e.StartDateAndTime<date2);
+        }
+        public bool screeningsOverlapping(int movieId, int cinemaId, DateTime screeningDate, int movieLength , int screeningId)
+        {   //check if there is another screening scheduled for the same time period
+            DateTime date1 = screeningDate.AddMinutes(-movieLength);
+            DateTime date2 = screeningDate.AddMinutes(movieLength);
+            return _context.Screenings
+                .Any(e => e.Id != screeningId && e.MovieId == movieId && e.CinemaId == cinemaId && e.StartDateAndTime > date1 && e.StartDateAndTime < date2);
         }
     }
 }
