e824383d81b525d11dffd4b0235b910e3c47ec25
Vassiliki Karagianniki <78561945+vassilikikrg@users.noreply.github.com>
2024-01-15 13:09:30 +0200

    Movie controller

---
+++ CinemaTicketBooking/Controllers/MoviesController.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using CinemaTicketBooking.Data;
+using CinemaTicketBooking.Models;
+using CinemaTicketBooking.ViewModels;
+
+namespace CinemaTicketBooking.Controllers
+{
+    public class MoviesController : Controller
+    {
+        private readonly CinemaDbContext _context;
+
+        public MoviesController(CinemaDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Movies
+        public async Task<IActionResult> Index()
+        {
+            var cinemaDbContext = _context.Movies.Include(m => m.ContentAdmin);
+            return View(await cinemaDbContext.ToListAsync());
+        }
+
+        // GET: Movies/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var movie = await _context.Movies
+                .Include(m => m.ContentAdmin)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (movie == null)
+            {
+                return NotFound();
+            }
+
+            return View(movie);
+        }
+        // GET: Movies/Create
+        public IActionResult Create()
+        {
+            //ViewData["ContentAdminId"] = new SelectList(_context.ContentAdmins, "Id", "Name");
+            return View();
+        }
+
+        // POST: Movies/Create
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Name,Content,Length,Summary,Director")] CreateMovieViewModel movieModel)
+        {
+
+            if (ModelState.IsValid)
+            {
+                Movie movie = new Movie { Name= movieModel.Name,
+                    Content=movieModel.Content,
+                    Length=movieModel.Length,
+                    Summary=movieModel.Summary,
+                    Director=movieModel.Director,
+                    ContentAdminId = 1 };//HARDCODED VALUE FOR NOW
+                _context.Add(movie);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(movieModel);
+        }
+
+
+        // GET: Movies/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var movie = await _context.Movies.FindAsync(id);
+            if (movie == null)
+            {
+                return NotFound();
+            }
+            ViewData["ContentAdminId"] = new SelectList(_context.ContentAdmins, "Id", "Id", movie.ContentAdminId);
+            return View(movie);
+        }
+
+        // POST: Movies/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Content,Length,Summary,Director,ContentAdminId")] Movie movie)
+        {
+            if (id != movie.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(movie);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!MovieExists(movie.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ContentAdminId"] = new SelectList(_context.ContentAdmins, "Id", "Id", movie.ContentAdminId);
+            return View(movie);
+        }
+
+        // GET: Movies/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var movie = await _context.Movies
+                .Include(m => m.ContentAdmin)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (movie == null)
+            {
+                return NotFound();
+            }
+
+            return View(movie);
+        }
+
+        // POST: Movies/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var movie = await _context.Movies.FindAsync(id);
+            if (movie != null)
+            {
+                _context.Movies.Remove(movie);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool MovieExists(int id)
+        {
+            return _context.Movies.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ CinemaTicketBooking/ViewModels/CreateMovieViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CinemaTicketBooking.ViewModels
+{
+    public class CreateMovieViewModel
+    {
+        [Required]
+        [StringLength(255)]
+        public string Name { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Content { get; set; }
+
+        [Required]
+        [Range(1, 1000)]
+        public int Length { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Summary { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Director { get; set; }
+    }
+
+}
